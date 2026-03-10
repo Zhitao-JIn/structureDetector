02d380b01e12b4afd2cfbe8990717d345e13ad10
Zhitao-Jin <zhitaojin2003@gmail.com>
2026-03-10 16:30:44 +0800

    <feat>(init): entry point

---
+++ src/main/java/com/zhitao/structuredetector/StructureDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.zhitao.structuredetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructureDetectorApplication.class, args);
+    }
+
+}
